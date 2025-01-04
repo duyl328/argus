a5c094bba82794d26ffdb10b240341244f46f7b3
charlatans <yllyou333@163.com>
2025-01-04 16:42:52 +0800

    更新todo

---
+++ argus-src/src/views/dev/vue/BackgroundTaskDev.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import CommandManager from '@/components/dev/CommandManager.vue'
+import type { CommandType } from '@/types/command.type'
+
+const commands: CommandType[] = [
+  {
+    name: 'add_task',
+    description: '添加任务',
+    params: [
+      {
+        name: 'task',
+        label: 'task',
+        type: 'text',
+        // 默认值
+        value: '添加任务',
+        // 提示字段，如：【输入名称】
+        placeholder: 'string'
+      }
+    ],
+    result: null
+  },
+  {
+    name: 'init_state',
+    description: 'init_state',
+    params: [
+      {
+        name: 'new_state',
+        label: 'new_state',
+        type: 'text',
+        // 默认值
+        value: 'init_state',
+        // 提示字段，如：【输入名称】
+        placeholder: 'string'
+      }
+    ],
+    result: null
+  }
+]
+</script>
+
+<template>
+  <CommandManager :pro="commands"></CommandManager>
+</template>
+
+<style scoped></style>
