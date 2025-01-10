d988689113fa27063d17a01208c5e988d788cff7
charlatans <yllyou333@163.com>
2025-01-10 16:35:21 +0800

    补充提交

--- argus-src/src-tauri/src/utils/exif_utils/tag.rs
+++ argus-src/src-tauri/src/utils/exif_utils/tag.rs
@@ -1,60 +1,25 @@
 use crate::tuples::Pair;
-use crate::utils::exif_utils::gps_util::GpsUtil;
+use crate::utils::exif_utils::gps_util::GpsInfo;
 use crate::utils::exif_utils::value::ValueType;
 use crate::utils::json_util::JsonUtil;
+use anyhow::{anyhow, Result};
+use chrono::{DateTime, FixedOffset, Utc};
 use std::borrow::Cow;
 use std::collections::HashMap;
-use std::fmt::format;
+use std::fmt;
+use std::str::FromStr;
 
 #[derive(Debug, Clone)]
-pub struct Tag {
+pub struct Tags {
     /// 原始数据保存
     pub entries: Vec<(String, String)>,
     /// 数据映射
     pub entry_map: HashMap<String, String>,
+    /// 遇到错误时继续
+    continue_on_error: bool,
 }
 
-/// 图像的 exif 信息对象
-pub struct ImgExif {
-    /// 相机制造商
-    make: String,
-    /// 相机型号
-    model: String,
-    /// 软件版本
-    software: String,
-    /// 曝光时间
-    exposure_time: String,
-    /// 闪光灯
-    flash: String,
-    /// 光圈
-    f_number: String,
-    /// ISO
-    iso: String,
-    /// exif 信息版本
-    // exif_version:String,
-    /// 创建日期
-    date_time_original: String,
-    /// 时区（+8）
-    offset_time: u32,
-    /// 最大光圈值
-    max_aperture_value: String,
-    /// 焦距
-    focal_length: String,
-    /// 宽度
-    image_width: String,
-    /// 长度
-    image_height: String,
-    /// gps 信息
-    gps_info: String,
-    /// 曝光程序
-    exposure_program: String,
-    /// 测光模式
-    metering_mode: String,
-    /// 作者（艺术家）
-    artist: String,
-}
-
-impl Tag {
+impl Tags {
     pub fn parse(mut self, info: &str) -> Self {
         for line in info.lines() {
             if let Some((key, value)) = line.split_once(':') {
@@ -67,12 +32,18 @@ impl Tag {
         self
     }
 
-    pub fn get(&self, key: &str) -> Option<Cow<String>> {
-        self.entry_map.get(key).map(|v| Cow::Borrowed(v))
+    pub fn is_empty(&self) -> bool {
+        self.entries.is_empty() || self.entry_map.is_empty()
+    }
+
+    pub fn get(&self, key: &str) -> Option<String> {
+        self.entry_map
+            .get(key)
+            .map(|v| Cow::Borrowed(v).to_string())
     }
 
     /// 打包数据
-    pub fn pack_tags(&self) -> anyhow::Result<String> {
+    pub fn pack_tags(&self) -> Result<String> {
         let mut res: Vec<Pair<String, String>> = Vec::new();
         ExifToolDesc::EXIF_INFOS_FRONT.map(|info| {
             let ans = self.get(info.exif_tool_desc);
@@ -88,41 +59,215 @@ impl Tag {
     }
 
     /// 打包为前端展示字段
-    pub fn pack_front_tags(&self) -> anyhow::Result<String> {
+    pub fn pack_front_tags(&self) -> Result<String> {
         let mut res: Vec<Pair<String, String>> = Vec::new();
+        let img_exif = self.pack_object()?;
 
         // 使用一个辅助函数处理字段的封装
-        let mut add_tag = |desc: &ExifInfo, field_name: &str| {
-            self.get(desc.exif_tool_desc).map(|x| {
-                res.push(Pair {
-                    first: field_name.to_string(),
-                    second: x.to_string(),
-                });
+        let mut add_tag = |desc: String, value: String| {
+            res.push(Pair {
+                first: desc,
+                second: value,
             });
         };
 
-        // 封装通用的字段添加逻辑
-        add_tag(&ExifToolDesc::MAKE, ExifToolDesc::MAKE.dis);
-        add_tag(&ExifToolDesc::MODEL, ExifToolDesc::MODEL.dis);
-        add_tag(&ExifToolDesc::SOFTWARE, ExifToolDesc::SOFTWARE.dis);
-        add_tag(&ExifToolDesc::ARTIST, ExifToolDesc::ARTIST.dis);
-        add_tag(&ExifToolDesc::FLASH, ExifToolDesc::FLASH.dis);
-        add_tag(&ExifToolDesc::FOCAL_LENGTH, ExifToolDesc::FOCAL_LENGTH.dis);
-        add_tag(
-            &ExifToolDesc::EXPOSURE_TIME,
-            ExifToolDesc::EXPOSURE_TIME.dis,
-        );
-        add_tag(&ExifToolDesc::F_NUMBER, ExifToolDesc::F_NUMBER.dis);
-        add_tag(&ExifToolDesc::ISO, ExifToolDesc::ISO.dis);
-        add_tag(
-            &ExifToolDesc::EXPOSURE_PROGRAM,
-            ExifToolDesc::EXPOSURE_PROGRAM.dis,
-        );
-        add_tag(
-            &ExifToolDesc::METERING_MODE,
-            ExifToolDesc::METERING_MODE.dis,
-        );
+        if let Some(x) = img_exif.make {
+            add_tag(ExifToolDesc::MAKE.dis.to_string(), x);
+        }
 
+        if let Some(x) = img_exif.software {
+            add_tag(ExifToolDesc::SOFTWARE.dis.to_string(), x);
+        }
+
+        if let Some(x) = img_exif.exposure_time {
+            add_tag(ExifToolDesc::EXPOSURE_TIME.dis.to_string(), x.to_string());
+        }
+
+        if let Some(x) = img_exif.flash {
+            add_tag(ExifToolDesc::FLASH.dis.to_string(), x);
+        }
+
+        if let Some(x) = img_exif.f_number {
+            add_tag(ExifToolDesc::F_NUMBER.dis.to_string(), x.to_string());
+        }
+
+        if let Some(x) = img_exif.iso {
+            add_tag(ExifToolDesc::ISO.dis.to_string(), x.to_string());
+        }
+
+        if let Some(x) = img_exif.date_time_original {
+            add_tag(
+                ExifToolDesc::DATE_TIME_ORIGINAL.dis.to_string(),
+                x.to_string(),
+            );
+        }
+
+        if let Some(x) = img_exif.max_aperture_value {
+            add_tag(ExifToolDesc::MAX_APERTURE_VALUE.dis.to_string(), x);
+        }
+
+        if let Some(x) = img_exif.focal_length {
+            add_tag(ExifToolDesc::FOCAL_LENGTH.dis.to_string(), x.to_string());
+        }
+
+        if let Some(x) = img_exif.image_width {
+            add_tag(ExifToolDesc::IMAGE_WIDTH.dis.to_string(), x.to_string());
+        }
+
+        if let Some(x) = img_exif.image_height {
+            add_tag(ExifToolDesc::IMAGE_HEIGHT.dis.to_string(), x.to_string());
+        }
+
+        if let Some(x) = img_exif.gps_info {
+            add_tag(String::from("GPS 信息"), x.to_string());
+        }
+
+        if let Some(x) = img_exif.exposure_program {
+            add_tag(ExifToolDesc::EXPOSURE_PROGRAM.dis.to_string(), x);
+        }
+
+        if let Some(x) = img_exif.metering_mode {
+            add_tag(ExifToolDesc::METERING_MODE.dis.to_string(), x);
+        }
+
+        if let Some(x) = img_exif.artist {
+            add_tag(ExifToolDesc::ARTIST.dis.to_string(), x);
+        }
+
+        JsonUtil::stringify(&res)
+    }
+
+    /// 打包为对象
+    pub fn pack_object(&self) -> Result<ImgExif> {
+        let make: Option<String>;
+        let model: Option<String>;
+        let software: Option<String>;
+        let exposure_time: Option<f64>;
+        let flash: Option<String>;
+        let f_number: Option<f64>;
+        let iso: Option<u32>;
+        let date_time_original: Option<DateTime<Utc>>;
+        let max_aperture_value: Option<String>;
+        let focal_length: Option<f64>;
+        let image_width: Option<u32>;
+        let image_height: Option<u32>;
+        let gps_info: Option<GpsInfo>;
+        let exposure_program: Option<String>;
+        let metering_mode: Option<String>;
+        let artist: Option<String>;
+
+        make = self.get(ExifToolDesc::MAKE.exif_tool_desc);
+        model = self.get(ExifToolDesc::MODEL.exif_tool_desc);
+        software = self.get(ExifToolDesc::SOFTWARE.exif_tool_desc);
+        artist = self.get(ExifToolDesc::ARTIST.exif_tool_desc);
+        flash = self.get(ExifToolDesc::FLASH.exif_tool_desc);
+        focal_length = self.parse_number_data1(ExifToolDesc::FOCAL_LENGTH.exif_tool_desc)?;
+        // 解析曝光时间信息
+        exposure_time = self.parse_exposure_time();
+        // 光圈数解析
+        f_number = self.parse_number_data1(ExifToolDesc::F_NUMBER.exif_tool_desc)?;
+        image_width = self.parse_number_data1(ExifToolDesc::IMAGE_WIDTH.exif_tool_desc)?;
+        image_height = self.parse_number_data1(ExifToolDesc::IMAGE_HEIGHT.exif_tool_desc)?;
+        max_aperture_value =
+            self.parse_number_data1(ExifToolDesc::MAX_APERTURE_VALUE.exif_tool_desc)?;
+        iso = self.parse_number_data1(ExifToolDesc::ISO.exif_tool_desc)?;
+        exposure_program = self.get(ExifToolDesc::EXPOSURE_PROGRAM.exif_tool_desc);
+        metering_mode = self.get(ExifToolDesc::METERING_MODE.exif_tool_desc);
+        // 解析 GPS
+        gps_info = Option::from(GpsInfo::parse(self, self.continue_on_error)?);
+        // 解析时间
+        date_time_original = self.parse_create_time();
+        Ok(ImgExif {
+            make,
+            model,
+            software,
+            exposure_time,
+            flash,
+            f_number,
+            iso,
+            date_time_original,
+            max_aperture_value,
+            focal_length,
+            image_width,
+            image_height,
+            gps_info,
+            exposure_program,
+            metering_mode,
+            artist,
+        })
+    }
+
+    /// 解析时间
+    pub fn parse_create_time(&self) -> Option<DateTime<Utc>> {
+        let create_time: Option<String> = self.get(ExifToolDesc::DATE_TIME_ORIGINAL.exif_tool_desc);
+        let offset_time: Option<String> = self.get(ExifToolDesc::OFFSET_TIME.exif_tool_desc);
+
+        // 如果 create_time 是 None，直接返回 None
+        let date_str = create_time?;
+
+        // 如果 offset_time 是 None，则使用默认的东八区时区 "+08:00"
+        let offset_str = offset_time.unwrap_or_else(|| "+08:00".to_string());
+
+        // 解析 Date/Time Original 字符串为 DateTime<FixedOffset>
+        let date_time = DateTime::parse_from_str(&date_str, "%Y:%m:%d %H:%M:%S").ok()?;
+
+        // 解析 Offset Time 字符串为 FixedOffset
+        let offset = FixedOffset::from_str(&offset_str).ok()?;
+
+        // 使用时区偏移创建 DateTime<FixedOffset>，然后转换为 UTC 时间
+        Some(date_time.with_timezone(&offset).with_timezone(&Utc))
+    }
+
+    // 解析数字类型数据
+    // pub fn parse_number_data<T>(&self, str: &str) -> Result<Option<T>> {
+    //     let ans = self.get(str);
+    //
+    //     if let Some(x) = ans {
+    //         let s: Result<T> = x.parse();
+    //         if self.continue_on_error {
+    //             if s.is_err() {
+    //                 Ok(None)
+    //             } else {
+    //                 Ok(Some(s.unwrap_or_default()))
+    //             }
+    //         } else {
+    //             Ok(Some(s?))
+    //         }
+    //     } else {
+    //         Ok(None)
+    //     }
+    // }
+
+    pub fn parse_number_data1<T>(&self, str: &str) -> Result<Option<T>>
+    where
+        T: FromStr + Default,
+    {
+        self.get(str).map_or(Ok(None), |x| match x.parse::<T>() {
+            Ok(value) => Ok(Some(value)),
+            Err(_) if self.continue_on_error => Ok(Some(T::default())),
+            Err(_e) => Err(anyhow!(format!("数据: {} 转换失败! ", x))),
+        })
+    }
+    /// 曝光时间
+    pub fn parse_exposure_time(&self) -> Option<f64> {
+        let option = self.get(ExifToolDesc::EXPOSURE_TIME.exif_tool_desc);
+        if let Some(x) = option {
+            let parts: Vec<&str> = x.split('/').collect();
+            if parts.len() == 2 {
+                let numerator: f64 = parts[0].parse().unwrap_or(1.0);
+                let denominator: f64 = parts[1].parse().unwrap_or(1.0);
+                Some(numerator / denominator)
+            } else {
+                // 如果格式不对
+                None
+            }
+        } else {
+            None
+        }
+    }
+
+    /// 焦距解析
+    pub fn parse_focal_length(&self) -> Option<String> {
         // 焦距和等效焦距处理
         if let Some(focal_length) = self.get(ExifToolDesc::FOCAL_LENGTH.exif_tool_desc) {
             let mm35 = self
@@ -134,104 +279,131 @@ impl Tag {
                 format!("{}, 等效焦距: {}", focal_length, mm35)
             };
 
-            res.push(Pair {
-                first: ExifToolDesc::FOCAL_LENGTH.dis.to_string(),
-                second: ans,
-            });
+            Some(ans)
+        } else {
+            None
         }
-
-        // GPS 信息拼接
-        let gps_info = self.parse_gps_tags().unwrap_or_else(|err| String::from(""));
-        res.push(Pair {
-            first: "GPS 信息".to_string(),
-            second: gps_info,
-        });
-
-        JsonUtil::stringify(&res)
-    }
-
-    /// 打包为对象
-    pub fn pack_object(&self)->ImgExif {
-        let make:String;
-        let model:String;
-        let software:String;
-        let exposure_time:String;
-        let flash:String;
-        let f_number:String;
-        let iso:String;
-        let date_time_original:String;
-        let offset_time:String;
-        let max_aperture_value:String;
-        let focal_length:String;
-        let image_width:String;
-        let image_height:String;
-        let gps_info:String;
-        let exposure_program:String;
-        let metering_mode:String;
-        let artist:String;
-
-        
-
-        todo!()
     }
 
     /// 解析 gps 数据【获取 gps 数据，并根据有无转换为文字信息】
-    pub fn parse_gps_tags(&self) -> anyhow::Result<String> {
-        // 经度
-        let longitude: String;
-        // 维度
-        let dimensions;
-        // 海拔
-        let altitude;
-
-        let gps_latitude = self.get(ExifToolDesc::GPS_LATITUDE.exif_tool_desc);
-        if gps_latitude.is_some() {
-            let gc = gps_latitude.unwrap_or_default().to_string();
-            let string = GpsUtil::resolve_coordinate(gc);
-
-            let gps_latitude_ref = self.get(ExifToolDesc::GPS_LATITUDE_REF.exif_tool_desc);
-            let gc_ref = if gps_latitude_ref.is_some() {
-                GpsUtil::resolve_direction(gps_latitude_ref.unwrap().to_string())
-            } else {
-                String::from("")
-            };
-
-            longitude = format!("{} {}", gc_ref, string)
-        } else {
-            longitude = String::from("")
-        }
-
-        let gps_longitude = self.get(ExifToolDesc::GPS_LONGITUDE.exif_tool_desc);
-        if gps_longitude.is_some() {
-            let gc = gps_longitude.unwrap_or_default().to_string();
-            let string = GpsUtil::resolve_coordinate(gc);
-
-            let gps_latitude_ref = self.get(ExifToolDesc::GPS_LONGITUDE_REF.exif_tool_desc);
-            let gc_ref = if gps_latitude_ref.is_some() {
-                GpsUtil::resolve_direction(gps_latitude_ref.unwrap().to_string())
-            } else {
-                String::from("")
-            };
-
-            dimensions = format!("{} {}", gc_ref, string)
-        } else {
-            dimensions = String::from("")
-        }
-
-        let gps_altitude = self
-            .get(ExifToolDesc::GPS_ALTITUDE.exif_tool_desc)
-            .unwrap_or_default()
-            .to_string();
-        altitude = GpsUtil::resolve_altitude(gps_altitude);
-
-        Ok(format!("{},{},{}", longitude, dimensions, altitude))
+    pub fn parse_gps_tags(&self) -> Result<GpsInfo> {
+        Ok(GpsInfo::parse(self, true)?)
     }
 
-    pub fn new() -> Self {
-        Tag {
+    pub fn new(continue_on_error: bool) -> Self {
+        Tags {
             entries: Vec::new(),
             entry_map: HashMap::new(),
+            continue_on_error,
         }
+    }
+}
+
+/// 图像的 exif 信息对象
+pub struct ImgExif {
+    /// 相机制造商
+    make: Option<String>,
+    /// 相机型号
+    model: Option<String>,
+    /// 软件版本
+    software: Option<String>,
+    /// 曝光时间
+    exposure_time: Option<f64>,
+    /// 闪光灯
+    flash: Option<String>,
+    /// 光圈
+    f_number: Option<f64>,
+    /// ISO
+    iso: Option<u32>,
+    /// exif 信息版本
+    // exif_version:OptionString>,
+    /// 创建日期
+    date_time_original: Option<DateTime<Utc>>,
+    /// 最大光圈值
+    max_aperture_value: Option<String>,
+    /// 焦距
+    focal_length: Option<f64>,
+    /// 宽度
+    image_width: Option<u32>,
+    /// 长度
+    image_height: Option<u32>,
+    /// gps 信息
+    gps_info: Option<GpsInfo>,
+    /// 曝光程序
+    exposure_program: Option<String>,
+    /// 测光模式
+    metering_mode: Option<String>,
+    /// 作者（艺术家）
+    artist: Option<String>,
+}
+
+impl fmt::Display for ImgExif {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut ans_str = String::new();
+        if let Some(x) = &self.make {
+            ans_str.push_str(x.as_str());
+        }
+
+        if let Some(x) = &self.model {
+            ans_str.push_str(x.as_str());
+        }
+
+        if let Some(x) = &self.software {
+            ans_str.push_str(x.as_str());
+        }
+
+        if let Some(x) = &self.exposure_time {
+            ans_str.push_str(x.to_string().as_str());
+        }
+
+        if let Some(x) = &self.flash {
+            ans_str.push_str(x.as_str());
+        }
+
+        if let Some(x) = &self.f_number {
+            ans_str.push_str(x.to_string().as_str());
+        }
+
+        if let Some(x) = &self.iso {
+            ans_str.push_str(x.to_string().as_str());
+        }
+
+        if let Some(x) = &self.date_time_original {
+            ans_str.push_str(x.to_string().as_str());
+        }
+
+        if let Some(x) = &self.max_aperture_value {
+            ans_str.push_str(x.to_string().as_str());
+        }
+
+        if let Some(x) = &self.focal_length {
+            ans_str.push_str(x.to_string().as_str());
+        }
+
+        if let Some(x) = &self.image_width {
+            ans_str.push_str(x.to_string().as_str());
+        }
+
+        if let Some(x) = &self.image_height {
+            ans_str.push_str(x.to_string().as_str());
+        }
+
+        if let Some(x) = &self.gps_info {
+            ans_str.push_str(x.to_string().as_str());
+        }
+
+        if let Some(x) = &self.exposure_program {
+            ans_str.push_str(x.to_string().as_str());
+        }
+
+        if let Some(x) = &self.metering_mode {
+            ans_str.push_str(x.to_string().as_str());
+        }
+
+        if let Some(x) = &self.artist {
+            ans_str.push_str(x.to_string().as_str());
+        }
+        write!(f, "{}", ans_str)
     }
 }
 
@@ -380,31 +552,7 @@ impl ExifToolDesc {
         &Self::ARTIST,
     ];
     /// 前端展示的数据
-    pub const EXIF_INFOS_FRONT: [&'static ExifInfo; 23] = [
-        &Self::MAKE,
-        &Self::MODEL,
-        &Self::SOFTWARE,
-        &Self::EXPOSURE_TIME,
-        &Self::F_NUMBER,
-        &Self::ISO,
-        &Self::EXIF_VERSION,
-        &Self::DATE_TIME_ORIGINAL,
-        &Self::OFFSET_TIME,
-        &Self::MAX_APERTURE_VALUE,
-        &Self::FOCAL_LENGTH,
-        &Self::FOCAL_LENGTH_IN_35MM_FORMAT,
-        &Self::IMAGE_WIDTH,
-        &Self::IMAGE_HEIGHT,
-        &Self::GPS_LATITUDE_REF,
-        &Self::GPS_LONGITUDE_REF,
-        &Self::GPS_LATITUDE,
-        &Self::GPS_LONGITUDE,
-        &Self::GPS_ALTITUDE,
-        &Self::EXPOSURE_PROGRAM,
-        &Self::METERING_MODE,
-        &Self::FLASH,
-        &Self::ARTIST,
-    ];
+    pub const EXIF_INFOS_FRONT: [&'static ExifInfo; 23] = ExifToolDesc::EXIF_INFOS;
 }
 
 #[derive(Clone, Debug)]
