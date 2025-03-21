2aed051f65c2f79f5d47fc9ada55b4fb90e50fbe
charlatans <yllyou333@163.com>
2025-03-21 13:52:07 +0800

    U:调整测试函数

--- argus-src/src/__test__/utils/fileUtil.test.ts
+++ argus-src/src/__test__/utils/fileUtil.test.ts
@@ -9,34 +9,34 @@
  *     c'est de voir le monde tel qu'il est et de l'aimer.
  */
 import { describe, expect, it, test } from 'vitest'
-import { formatFileSize } from '@/utils/fileUtil'
+import { convertFileSize } from '@/utils/fileUtil'
 import { FILE_SIZE_UNIT_ENUM } from '@/constants/enums'
 
 describe('constants.ts', () => {
   it('should ', () => {
     expect(() => {
-      formatFileSize(-1, FILE_SIZE_UNIT_ENUM.B)
+      convertFileSize(-1, FILE_SIZE_UNIT_ENUM.B)
     }).toThrow()
   })
   it('数据单位选择', () => {
-    console.log(formatFileSize(0, FILE_SIZE_UNIT_ENUM.B))
-    console.log(formatFileSize(1, FILE_SIZE_UNIT_ENUM.B))
-    console.log(formatFileSize(100, FILE_SIZE_UNIT_ENUM.B))
-    console.log(formatFileSize(1000, FILE_SIZE_UNIT_ENUM.B))
-    console.log(formatFileSize(10000, FILE_SIZE_UNIT_ENUM.B))
-    console.log(formatFileSize(100000, FILE_SIZE_UNIT_ENUM.B))
-    console.log(formatFileSize(1000000, FILE_SIZE_UNIT_ENUM.B))
-    console.log(formatFileSize(10000000, FILE_SIZE_UNIT_ENUM.B))
-    console.log(formatFileSize(100000000, FILE_SIZE_UNIT_ENUM.B))
-    console.log(formatFileSize(1000000000, FILE_SIZE_UNIT_ENUM.B))
-    console.log(formatFileSize(10000000000, FILE_SIZE_UNIT_ENUM.B))
-    console.log(formatFileSize(100000000000, FILE_SIZE_UNIT_ENUM.B))
-    console.log(formatFileSize(1000000000000, FILE_SIZE_UNIT_ENUM.B))
-    console.log(formatFileSize(10000000000000, FILE_SIZE_UNIT_ENUM.B))
-    console.log(formatFileSize(100000000000000, FILE_SIZE_UNIT_ENUM.B))
-    console.log(formatFileSize(1000000000000000, FILE_SIZE_UNIT_ENUM.B))
-    console.log(formatFileSize(10000000000000000, FILE_SIZE_UNIT_ENUM.B))
-    console.log(formatFileSize(100000000000000000, FILE_SIZE_UNIT_ENUM.B))
-    console.log(formatFileSize(1000000000000000000, FILE_SIZE_UNIT_ENUM.B))
+    console.log(convertFileSize(0, FILE_SIZE_UNIT_ENUM.B))
+    console.log(convertFileSize(1, FILE_SIZE_UNIT_ENUM.B))
+    console.log(convertFileSize(100, FILE_SIZE_UNIT_ENUM.B))
+    console.log(convertFileSize(1000, FILE_SIZE_UNIT_ENUM.B))
+    console.log(convertFileSize(10000, FILE_SIZE_UNIT_ENUM.B))
+    console.log(convertFileSize(100000, FILE_SIZE_UNIT_ENUM.B))
+    console.log(convertFileSize(1000000, FILE_SIZE_UNIT_ENUM.B))
+    console.log(convertFileSize(10000000, FILE_SIZE_UNIT_ENUM.B))
+    console.log(convertFileSize(100000000, FILE_SIZE_UNIT_ENUM.B))
+    console.log(convertFileSize(1000000000, FILE_SIZE_UNIT_ENUM.B))
+    console.log(convertFileSize(10000000000, FILE_SIZE_UNIT_ENUM.B))
+    console.log(convertFileSize(100000000000, FILE_SIZE_UNIT_ENUM.B))
+    console.log(convertFileSize(1000000000000, FILE_SIZE_UNIT_ENUM.B))
+    console.log(convertFileSize(10000000000000, FILE_SIZE_UNIT_ENUM.B))
+    console.log(convertFileSize(100000000000000, FILE_SIZE_UNIT_ENUM.B))
+    console.log(convertFileSize(1000000000000000, FILE_SIZE_UNIT_ENUM.B))
+    console.log(convertFileSize(10000000000000000, FILE_SIZE_UNIT_ENUM.B))
+    console.log(convertFileSize(100000000000000000, FILE_SIZE_UNIT_ENUM.B))
+    console.log(convertFileSize(1000000000000000000, FILE_SIZE_UNIT_ENUM.B))
   })
 })
